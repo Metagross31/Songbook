d502da3db85f794123a42e33b9979c9cbbb16533
Stephan Hagel <sthagel@uw.edu>
2020-03-10 21:42:07 +0100

    Added more replacement rules to UGloader.sh
    The UGloader.sh script now also checks for the following escape sequences and
    replaced them with their UTF-8 counterparts:
    &ouml;		->	ö
    &auml;		->	ä
    &uuml;		->	ü
    &szlig;		->	ß
    &quot;		->	"

--- UGloader.sh
+++ UGloader.sh
@@ -11,7 +11,7 @@ wget -O temp -U 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.6) Gecko/20070
 
 SONG_TITLE=$(cat temp | grep "<div class=\"js-store\" data-content" | awk -F'song_name&quot;:&quot;' '{print $2}' | awk -F'&quot;' '{print $1}') 
 ARTIST_NAME=$(cat temp | grep "<div class=\"js-store\" data-content" | awk -F'artist_name&quot;:&quot;' '{print $2}' | awk -F'&quot;' '{print $1}')
-SONG_CONTENT=$(cat temp | grep "<div class=\"js-store\" data-content" | awk -F'content&quot;:&quot;' '{print $2}' | awk -F'&quot;' '{print $1}')
+SONG_CONTENT=$(cat temp | grep "<div class=\"js-store\" data-content" | awk -F'content&quot;:&quot;' '{print $2}' | awk -F'&quot;,&quot;' '{print $1}')
 
 rm temp
 
@@ -19,6 +19,21 @@ SONG_CONTENT=$(echo $SONG_CONTENT | sed -e s/'\[tab\]'//g)
 SONG_CONTENT=$(echo $SONG_CONTENT | sed -e s/'\[\/tab\]'//g)
 SONG_CONTENT=$(echo $SONG_CONTENT | sed -e s/'\[ch\]'//g)
 SONG_CONTENT=$(echo $SONG_CONTENT | sed -e s/'\[\/ch\]'//g)
+SONG_CONTENT=$(echo $SONG_CONTENT | sed -e s/'\&ouml;'/'ö'/g)
+SONG_CONTENT=$(echo $SONG_CONTENT | sed -e s/'\&auml;'/'ä'/g)
+SONG_CONTENT=$(echo $SONG_CONTENT | sed -e s/'\&uuml;'/'ü'/g)
+SONG_CONTENT=$(echo $SONG_CONTENT | sed -e s/'\&szlig;'/'ß'/g)
+SONG_CONTENT=$(echo $SONG_CONTENT | sed -e s/'\\\&quot;'/'"'/g)
+
+ARTIST_NAME=$(echo $ARTIST_NAME | sed -e s/'\&ouml;'/'ö'/g)
+ARTIST_NAME=$(echo $ARTIST_NAME | sed -e s/'\&auml;'/'ä'/g)
+ARTIST_NAME=$(echo $ARTIST_NAME | sed -e s/'\&uuml;'/'ü'/g)
+ARTIST_NAME=$(echo $ARTIST_NAME | sed -e s/'\&szlig;'/'ß'/g)
+
+SONG_TITLE=$(echo $SONG_TITLE | sed -e s/'\&ouml;'/'ö'/g)
+SONG_TITLE=$(echo $SONG_TITLE | sed -e s/'\&auml;'/'ä'/g)
+SONG_TITLE=$(echo $SONG_TITLE | sed -e s/'\&uuml;'/'ü'/g)
+SONG_TITLE=$(echo $SONG_TITLE | sed -e s/'\&szlig;'/'ß'/g)
 
 FILENAME=${SONG_TITLE}_${ARTIST_NAME}.tex
 
